8c48361f9977584b2cc79c3aba058fa855fcd648
Dennis Kruemmel <dennis@kruemmel.it>
2014-11-12 21:41:18 +0100

    added path variables
    escaped some variables

--- mvmails.sh
+++ mvmails.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
+BASEPATH="/home/kit/users/dennis"
+
 logfile="/home/kit/bin/logs/mvmails.log"
 
-echo `date` --- >> $logfile
+echo "`date` ---" >> $logfile
 
-if [ "$1" == "" ]; then
-  echo var1 ist nicht gesetzt
+if [ -z "$1" ]; then
+  echo "var1 ist nicht gesetzt"
 else
-  echo `date` - input filename: $1 >> $logfile
-  echo `date` - cutted filename: ${1%:*} >> $logfile
+  echo "`date` - input filename: ${1}" >> $logfile
+  echo "`date` - cutted filename: ${1%:*}" >> $logfile
   subject=""
   subject=`grep "^Subject:" $1`
-  echo `date` - $subject >> $logfile
-  cp $1 /home/kit/users/dennis/._Wiedervorlage.9_Archiv/cur/`date +%s%N`:2,S
-  mv -f $1 ${1%:*}
-  mv -f ${1%:*} /home/kit/users/dennis/new/
+  echo "`date` - ${subject}" >> $logfile
+  cp "${1}" "${BASEPATH}/._Wiedervorlage.9_Archiv/cur/`date +%s%N`:2,S"
+  mv -f "${1}" "${1%:*}"
+  mv -f "${1%:*}" "${BASEPATH}/new/"
 fi
-
