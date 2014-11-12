daf8c7afd4ff7b71cd44c7ab66e0dd50ea736a4e
Dennis Kruemmel <dennis@kruemmel.it>
2014-11-12 22:13:01 +0100

    removed "" in find -exec

--- cleanmails.sh
+++ cleanmails.sh
@@ -15,41 +15,41 @@ find "${BASEPATH}/._Wiedervorlage.9_Archiv/cur" -mtime +60 -delete
 
 #Wiedervorlage
 #Test 1 Minute
-#find "${BASEPATH}/._Wiedervorlage.1_1h/cur/" -type f -cmin +1 -exec "~/bin/mvmails.sh" {} \;
+#find "${BASEPATH}/._Wiedervorlage.1_1h/cur/" -type f -cmin +1 -exec ~/bin/mvmails.sh {} \;
 
-#abend
+#Abends
 if [ $TIME -ge 17 ] && [ $TIME -le 23 ]; then
-  find "${BASEPATH}/._Wiedervorlage.0_abends/cur/" -type f -cmin +1 -exec "~/bin/mvmails.sh" {} \;
+  find "${BASEPATH}/._Wiedervorlage.0_abends/cur/" -type f -cmin +1 -exec ~/bin/mvmails.sh {} \;
 fi
 
 #Wochenende
 if [ $TIME -ge 17 ] && [ $TIME -le 23 ] && [ $WEEKDAY -ge 5 ]; then
-  find "${BASEPATH}/._Wiedervorlage.0_Wochenende/cur/" -type f -cmin +1 -exec "~/bin/mvmails.sh" {} \;
+  find "${BASEPATH}/._Wiedervorlage.0_Wochenende/cur/" -type f -cmin +1 -exec ~/bin/mvmails.sh {} \;
 fi
 
 #1 Stunde
-find "${BASEPATH}/._Wiedervorlage.1_1h/cur/" -type f -cmin +60 -exec "~/bin/mvmails.sh" {} \;
+find "${BASEPATH}/._Wiedervorlage.1_1h/cur/" -type f -cmin +60 -exec ~/bin/mvmails.sh {} \;
 
 #6 Stunden
-find "${BASEPATH}/._Wiedervorlage.1_6h/cur/" -type f -cmin +360 -exec "~/bin/mvmails.sh" {} \;
+find "${BASEPATH}/._Wiedervorlage.1_6h/cur/" -type f -cmin +360 -exec ~/bin/mvmails.sh {} \;
 
 #12 Stunden
-find "${BASEPATH}/._Wiedervorlage.1_12h/cur/" -type f -cmin +720 -exec "~/bin/mvmails.sh" {} \;
+find "${BASEPATH}/._Wiedervorlage.1_12h/cur/" -type f -cmin +720 -exec ~/bin/mvmails.sh {} \;
 
 #1 Tag
-find "${BASEPATH}/._Wiedervorlage.2_1d/cur/" -type f -cmin +1440 -exec "~/bin/mvmails.sh" {} \;
+find "${BASEPATH}/._Wiedervorlage.2_1d/cur/" -type f -cmin +1440 -exec ~/bin/mvmails.sh {} \;
 
 #2 Tage
-find "${BASEPATH}/._Wiedervorlage.2_2d/cur/" -type f -ctime +1 -exec "~/bin/mvmails.sh" {} \;
+find "${BASEPATH}/._Wiedervorlage.2_2d/cur/" -type f -ctime +1 -exec ~/bin/mvmails.sh {} \;
 
 #3 Tage
-find "${BASEPATH}/._Wiedervorlage.2_3d/cur/" -type f -ctime +2 -exec "~/bin/mvmails.sh" {} \;
+find "${BASEPATH}/._Wiedervorlage.2_3d/cur/" -type f -ctime +2 -exec ~/bin/mvmails.sh {} \;
 
 #1 Woche
-find "${BASEPATH}/._Wiedervorlage.3_1w/cur/" -type f -ctime +6 -exec "~/bin/mvmails.sh" {} \;
+find "${BASEPATH}/._Wiedervorlage.3_1w/cur/" -type f -ctime +6 -exec ~/bin/mvmails.sh {} \;
 
 #2 Wochen
-find "${BASEPATH}/._Wiedervorlage.3_2w/cur/" -type f -ctime +13 -exec "~/bin/mvmails.sh" {} \;
+find "${BASEPATH}/._Wiedervorlage.3_2w/cur/" -type f -ctime +13 -exec ~/bin/mvmails.sh {} \;
 
 #1 Monat
-find "${BASEPATH}/._Wiedervorlage.4_1m/cur/" -type f -ctime +29 -exec "~/bin/mvmails.sh" {} \;
+find "${BASEPATH}/._Wiedervorlage.4_1m/cur/" -type f -ctime +29 -exec ~/bin/mvmails.sh {} \;
